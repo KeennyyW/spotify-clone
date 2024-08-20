d96aef4baf4c4d9b80127b69e356e7de78de92d1
Laurin04 <laurinlattanzi12@gmail.com>
2024-08-20 15:47:16 +0200

    implemented login, logout and register using the django auth system

--- spotify/music/views.py
+++ spotify/music/views.py
@@ -1,7 +1,88 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
+from django.contrib.auth.decorators import login_required 
+from django.contrib.auth import logout, authenticate, login
+
 #from django.http import HttpResponse  
 # Create your views here.
 
-
+@login_required(login_url="login")
 def index(request):
     return render(request, "music/index.html")
+
+
+def login(request):
+    if request.method == "POST":
+        
+        # gets username and password via POST data
+
+        username = request.POST.get("username") 
+        password = request.POST.get("password")
+
+        # checks if user is already registered 
+
+        user = authenticate(request, username=username, password=password)
+
+        # if the user exists they will be logged in and redirected
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect("index")
+        else:
+            messages.info(request, "User not found")
+            return redirect("login")
+        
+    else:
+        return render(request, "music/login.html")
+
+def signup(request):
+    if request.method == "POST":
+
+        #defining userdata
+
+        username = request.POST.get("username") 
+        email = request.POST.get("email") 
+        password = request.POST.get("password") 
+        password2 = request.POST.get("password2") 
+
+        # check if password is the same
+
+        if password2 == password:
+
+            # check if the email or the username is already in the database
+
+            if User.objects.filter(email=email).exists():
+                messages.info(request, "Email already taken")
+                return redirect("signup")
+            
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, "Username already taken")
+                return redirect("signup")
+            
+            # creates new user 
+
+            else: 
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+            
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+                return redirect("/")
+        else: 
+            messages.info(request, "Password Not Matching")
+            return redirect("signup")
+
+    else:
+        return render(request, "music/signup.html")
+
+
+@login_required(login_url="login")
+def logout_func(request):
+    auth.logout(request)
+    return redirect("signup")
+
+
+
+def logout():
+    pass
--- spotify/spotify/settings.py
+++ spotify/spotify/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,11 +75,23 @@ WSGI_APPLICATION = 'spotify.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'ssusbSFWqWgZsFuEtOGYadaWWMVJByfz',
+#         'HOST': 'postgres.railway.internal',
+#         'PORT': '5432',
+#     }
+# }
+
 
 
 # Password validation
@@ -117,6 +129,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "music/static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
