2b335a8168469a870cdc15f155b3fe00dfc4750a
KeennyyW <laurinlatt9@gmail.com>
2024-09-08 12:58:29 +0200

    test

--- spotify/music/urls.py
+++ spotify/music/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('signup', views.signup, name="signup"),
     path('login', views.login, name="login"),
     path('logout', views.logout_func, name="logout"),
-    path('test', views.test, name="test"),
+    #path('test', views.test, name="test"),
     
     
     
